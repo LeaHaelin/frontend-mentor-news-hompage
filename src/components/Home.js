a15fd4f7f74b5214266fedbab9d483101ace67fb
LeaHaelin <lea8haelin@gmail.com>
2023-02-22 23:42:22 +0100

    refactor home styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import homeImg from '../assets/images/image-web-3-mobile.jpg';
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <picture>
+                <img className='home__header' src={homeImg} alt="" />
+            </picture>
+            <section className="home__contents">
+                <h1 className="home__contents--title">The Bright Future of Web 3.0?</h1>
+                <p className="home__contents--text">We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+                <button className="home__contents--btn">read more</button>
+            </section>
+        </div>
     )
 }
